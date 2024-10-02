33e053380da643b8cb03f8643315db35a617ea61
Naio200000 <nicolas.alsinet@gmail.com>
2024-10-02 12:52:56 -0300

    feat: add /materia response

--- deafio-4/server.js
+++ deafio-4/server.js
@@ -3,12 +3,28 @@ import express from 'express'
 const app = express()
 const PORT = 3000;
 
+const materia = {
+    id: '2044',
+    name: 'Aplicaciones Hibridas',
+    prof: 'Camila Belén Marcos Galban',
+    dias: [
+        {dia: 'Martes', hora: '21:10'},
+        {dia: 'Miercoles', hora: '21:10'}
+    ]
+}
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
 
     res.status(200)
         .send('Nicolas Alsinet')
+})
+
+app.get('/materia', (req, res) => {
+
+    res.status(200)
+        .json(materia)
 })
 
 app.listen(PORT, () => console.log('El Servidor está funcionando'))
